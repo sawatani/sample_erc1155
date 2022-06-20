cf90050df513acd981087497622956487432f420
沢谷 邦夫 <devel@fathens.org>
2022-06-20 12:52:48 +0900

    Impl `mint` and `mintBatch`

--- contracts/OwnableERC1155.sol
+++ contracts/OwnableERC1155.sol
@@ -16,7 +16,21 @@ contract OwnableERC1155 is ERC1155, Ownable {
         _setURI(_uri);
     }
 
-    function mint( address to, uint256 id) public onlyOwner {
-        _mint(to, id, 0, "");
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(to, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
     }
 }
